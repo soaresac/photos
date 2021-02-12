a72b38e7f6046ab3afff267a2a8e9c9da868d117
Amanda Soares <soaresac>
2021-02-12 15:07:49 -0300

    parte 1 e 2 curso de Angular

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { SignUpService } from './signup/signup.service';
+
+@NgModule({
+  declarations: [SigninComponent, SignupComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VmessageModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    SignUpService
+  ]
+})
+export class HomeModule { } 
